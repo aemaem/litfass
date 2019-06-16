80540c403130a4ee56b37f250c13a03b02dd0e49
Michael Mair <michael.mair@leftshift.one>
2019-06-16 10:33:00 +0200

    http service now only accepts json response

--- litfass-server/src/main/kotlin/lit/fass/litfass/server/http/CollectionHttpService.kt
+++ litfass-server/src/main/kotlin/lit/fass/litfass/server/http/CollectionHttpService.kt
@@ -2,12 +2,14 @@ package lit.fass.litfass.server.http
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.http.HttpHeaders.ACCEPT
 import org.apache.http.HttpHeaders.AUTHORIZATION
 import org.apache.http.client.methods.CloseableHttpResponse
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.HttpClientBuilder.create
 import org.apache.http.util.EntityUtils.toByteArray
 import org.slf4j.LoggerFactory
+import org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE
 import org.springframework.stereotype.Service
 import java.util.*
 
@@ -23,6 +25,7 @@ class CollectionHttpService(private val jsonMapper: ObjectMapper) : HttpService 
     override fun get(url: String, username: String?, password: String?): Map<String, Any?> {
         log.info("Executing http get request on $url")
         val request = HttpGet(url)
+        request.addHeader(ACCEPT, APPLICATION_JSON_UTF8_VALUE)
         request.addHeader(AUTHORIZATION, "Basic ${base64Encode("$username:$password")}")
 
         var response: CloseableHttpResponse? = null
---
+++ litfass-server/src/test/groovy/lit/fass/litfass/server/http/CollectionHttpServiceSpec.groovy
@@ -0,0 +1,20 @@
+package lit.fass.litfass.server.http
+
+import lit.fass.config.ServerConfiguration
+import lit.fass.litfass.server.helper.ManualTest
+import org.junit.experimental.categories.Category
+import spock.lang.Specification
+
+/**
+ * @author Michael Mair
+ */
+@Category(ManualTest)
+class CollectionHttpServiceSpec extends Specification {
+
+    def "test"() {
+        expect:
+        def result = new CollectionHttpService(new ServerConfiguration().jsonMapper())
+                .get("https://circleci.com/api/v1.1/project/github/leftshiftone/keios-protocol/125", "553f9c18552c7314bdfdc7740b7febe05e0ea5a9", "")
+        result
+    }
+}
